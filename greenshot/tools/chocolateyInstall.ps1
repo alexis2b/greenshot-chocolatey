be0e4c2ceee698d177a9e02d1ab63d047c660029
Terje With Lunndal <121591464+lunndal@users.noreply.github.com>
2025-11-10 19:14:43 +0100

    fix(installer): Make installer install for all users, using /ALLUSERS

--- greenshot/tools/chocolateyInstall.ps1
+++ greenshot/tools/chocolateyInstall.ps1
@@ -17,8 +17,9 @@ $packageArgs = @{
   checksum64    = ''
   checksumType64= 'sha256'
 
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs   = '/ALLUSERS /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
+
